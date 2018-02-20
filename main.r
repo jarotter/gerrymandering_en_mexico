c4e8904e958be480d1ff24fe0c20d2c7423f8765
Jorge Rotter <jorgearotter@gmail.com>
2018-02-20 13:07:37 -0600

    Creamos medidas

--- main.r
+++ main.r
@@ -31,8 +31,9 @@ distritaciones <- cdmx$ine18 %>%
   as.tibble()
 names(distritaciones) <- 'ine18'
 
-num_distritaciones <- 2
+num_distritaciones <- 10
 for(i in 1:num_distritaciones){
+  print(i)
   xi <- take_one_sample(cdmx_graph, neighbors, cdmx, wd, wp, wi, dict, perimetros_iniciales, conteo_delegaciones)
   colname <- paste('p',ncol(distritaciones), sep="")
   distritaciones <- cbind(distritaciones, xi)
